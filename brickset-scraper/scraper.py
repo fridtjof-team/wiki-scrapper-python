bb4650691a55ad22a8d04e1235f3ee2a9619efb6
Sritam Patnaik <sritampatnaik@gmail.com>
2018-12-11 20:50:28 +0800

    remove scraper env from repo

--- brickset-scraper/scraper.py
+++ brickset-scraper/scraper.py
@@ -1,6 +1,11 @@
 import scrapy
 
-
-class BrickSetSpider(scrapy.Spider):
-    name = "brickset_spider"
-    start_urls = ['https://en.wikipedia.org/w/api.php?action=parse&prop=text&page=Category:Visa_requirements_by_nationality&format=php']
+class CountryDataSpider(scrapy.Spider):
+    name = "country_data_spider"
+    start_urls = ['https://en.wikipedia.org/wiki/Template:Visa_requirements']
+    
+    def parse(self, response):
+        for country in response.css('div/ul/li'):
+            yield {
+                'text': country.css('a::text').extract_first(),
+            }
--- visa-scraper/scraper.py
+++ visa-scraper/scraper.py
@@ -1,6 +1,11 @@
 import scrapy
 
-
-class BrickSetSpider(scrapy.Spider):
-    name = "brickset_spider"
-    start_urls = ['https://en.wikipedia.org/wiki/Category:Visa_requirements_by_nationality']
+class CountryDataSpider(scrapy.Spider):
+    name = "country_data_spider"
+    start_urls = ['https://en.wikipedia.org/wiki/Template:Visa_requirements']
+    
+    def parse(self, response):
+        for country in response.css('div/ul/li'):
+            yield {
+                'text': country.css('a::text').extract_first(),
+            }
